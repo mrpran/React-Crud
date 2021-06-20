1a9194fbc355529afe86b9e881ca935b8c529dc6
mrpran <pran05412@gmail.com>
2021-06-20 21:06:14 +0530

    form completed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import {userService} from '../services/user.service';
+
+
+import { userService } from '../services/user.service';
 
 function Home() {
 
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,11 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
+
 
 function Users() {
+
+    const validationSchema = Yup.object().shape({
+        title: Yup.string()
+            .required('Title is required'),
+        body: Yup.string()
+            .required('Body is required'),
+    });
+
+    const { register, handleSubmit, reset, setValue, formState: { errors }, formState } = useForm({
+        resolver: yupResolver(validationSchema)
+    });
+
+    function onSubmit(data) {
+        console.log(data);
+    }
+
+    const [user, setUser] = useState({});
+
     return (
-        <div>
-            users
-        </div>
-    )
+        <form onSubmit={handleSubmit(onSubmit)} onReset={reset}>
+            <div className="form-row">
+                <div className="form-group col-5">
+                    <label>Title</label>
+                    <input {...register("title")}type="text" className={`form-control ${errors.title ? 'is-invalid' : ''}`} />
+                    <div className="invalid-feedback">{errors.title?.message}</div>
+                </div>
+                <div className="form-group col-5">
+                    <label>Body</label>
+                    <input {...register("body")} type="text" className={`form-control ${errors.body ? 'is-invalid' : ''}`} />
+                    <div className="invalid-feedback">{errors.body?.message}</div>
+                </div>
+            </div>
+            <div className="form-group">
+                <button type="submit" disabled={formState.isSubmitting} className="btn btn-primary">
+                    {formState.isSubmitting && <span className="spinner-border spinner-border-sm mr-1"></span>}
+                    Save
+                </button>
+            </div>
+        </form>
+    );
 }
 
 export default Users
